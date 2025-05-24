adffc3799808d94caa2dbafa3373b80ef498c3ec
olushola <ilesanmiolushola9@gmail.com>
2025-05-24 12:28:21 +0100

    updates cors

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -25,9 +25,10 @@ class Tenant extends BaseTenant
     {
         static::creating(function ($tenant) {
             if (!app()->environment('production')) {
-                $tenant->database = 'jkpmjemy_tenant_' . Str::slug($tenant->name, '_') . '_' . Str::random(4);
+                // $tenant->database = 'jkpmjemy_tenant_' . Str::slug($tenant->name, '_') . '_' . Str::random(4);
+                $tenant->database = 'tenant_' . Str::slug($tenant->name, '_');
             } elseif (empty($tenant->database)) {
-                $tenant->database = 'tenant_john_hospital';
+                $tenant->database = 'jkpmjemy_tenant_john_hospital';
             }
         });
     }
--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,15 @@
 <?php
 
 return [
-
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
         'http://localhost:5173',
+        'https://emed-application.vercel.app',
     ],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
