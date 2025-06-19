b934fc249b2058585ba8eb9ef30b6e36b6512601
StephenOkpeku <Stephen@appentus.in>
2025-06-19 18:11:29 +0100

    update

--- app/Repositories/MedicationInventory/MedicationInventoryRepository.php
+++ app/Repositories/MedicationInventory/MedicationInventoryRepository.php
@@ -129,41 +129,43 @@ class MedicationInventoryRepository implements MedicationInventoryRepositoryInte
 
     //    return $paginated->getCollection(); 
 
-    $shipments = $paginated->getCollection()->map(function ($item) {
-          $medication = optional($item->medication);
-    $pharmacy = optional($item->pharmacy);
-    $sellingPrice = $medication->selling_price ?? 0;
-    $totalPrice = $item->received_qty * $sellingPrice; 
-    return [
-    'id' => $item->id, 
-    'BatchNo' => $item->batch_no, 
-    'shipment_no'=>$item->shipment_no,
-    'ShipmentStatus' => $item->shipment_status,
-    'MedicineName' => $medication->medicine_name,
-    'BrandName' => $item->brand_name,
-    'GenericName' => $medication->generic_name,
-    'Pharmacy' => $pharmacy->name,
-    'ReceivedQty' => $item->received_qty,
-    'SellingPrice' => (float) $sellingPrice,
-    'TotalPrice' => (float) $totalPrice,
-    'CreatedAt' => optional($item->created_at)->toDateTimeString(),
+    // $shipments = $paginated->getCollection()->map(function ($item) {
+    //       $medication = optional($item->medication);
+    // $pharmacy = optional($item->pharmacy);
+    // $sellingPrice = $medication->selling_price ?? 0;
+    // $totalPrice = $item->received_qty * $sellingPrice; 
+    // return [
+    // 'id' => $item->id, 
+    // 'BatchNo' => $item->batch_no, 
+    // 'shipment_no'=>$item->shipment_no,
+    // 'ShipmentStatus' => $item->shipment_status,
+    // 'MedicineName' => $medication->medicine_name,
+    // 'BrandName' => $item->brand_name,
+    // 'GenericName' => $medication->generic_name,
+    // 'Pharmacy' => $pharmacy->name,
+    // 'ReceivedQty' => $item->received_qty,
+    // 'SellingPrice' => (float) $sellingPrice,
+    // 'TotalPrice' => (float) $totalPrice,
+    // 'CreatedAt' => optional($item->created_at)->toDateTimeString(),
     
-    ];
+    // ];
 
-    });
-    $shipments[] = [
-        'current_page' => $paginated->currentPage(),
-        'last_page' => $paginated->lastPage(),
-        'per_page' => $paginated->perPage(),
-        'total' => $paginated->total(),
-    ];
-     $shipments[] = [
-        'first' => $paginated->url(1),
-        'last' => $paginated->url($paginated->lastPage()),
-        'prev' => $paginated->previousPageUrl(),
-        'next' => $paginated->nextPageUrl(),
-     ];
-    return $shipments;
+    // });
+    // $shipments[] = [
+    //     'current_page' => $paginated->currentPage(),
+    //     'last_page' => $paginated->lastPage(),
+    //     'per_page' => $paginated->perPage(),
+    //     'total' => $paginated->total(),
+    // ];
+    //  $shipments[] = [
+    //     'first' => $paginated->url(1),
+    //     'last' => $paginated->url($paginated->lastPage()),
+    //     'prev' => $paginated->previousPageUrl(),
+    //     'next' => $paginated->nextPageUrl(),
+    //  ];
+    // return $shipments;
+
+      return $paginated->getCollection();
     }
 
 
