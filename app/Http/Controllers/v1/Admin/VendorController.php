54e897bdaf70cd492a847ffc8ebcd38e4d4004f5
StephenOkpeku <Stephen@appentus.in>
2025-06-19 20:41:47 +0100

    update

--- app/Http/Controllers/v1/Admin/VendorController.php
+++ app/Http/Controllers/v1/Admin/VendorController.php
@@ -145,11 +145,13 @@ class VendorController extends Controller
 
     public function update_status(UpdateStatusVendorRequest $request, $id){
    //  try {
-        
+         DB::connection('landlord')->beginTransaction();
          $validated = $request->validated();
           $result = $this->service->update_status($validated, $id);
+           DB::connection('landlord')->commit();
         return JsonResponser::send(false, 'Vendor stats fetched successfully', $result);
     //  } catch (\Throwable $th) {
+    // DB::connection('landlord')->rollBack();
     //  return JsonResponser::send(true, 'Internal server error', [], 500, $th);
     //  }
     }
