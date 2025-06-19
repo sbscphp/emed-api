24d71eb117d48a4cde93d4c535960f9a76348bcf
StephenOkpeku <Stephen@appentus.in>
2025-06-19 16:27:39 +0100

    update

--- app/Repositories/MedicationInventory/MedicationInventoryRepository.php
+++ app/Repositories/MedicationInventory/MedicationInventoryRepository.php
@@ -141,7 +141,7 @@ class MedicationInventoryRepository implements MedicationInventoryRepositoryInte
           ];
         }
 
-        return $arr;
+        return response()->json($arr);
     }
 
 
