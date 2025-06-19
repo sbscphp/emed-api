3f7bf62b58aeb644765c95e4d0df7276a1c2f80b
StephenOkpeku <Stephen@appentus.in>
2025-06-19 14:54:08 +0100

    update

--- app/Repositories/MedicationInventory/MedicationInventoryRepository.php
+++ app/Repositories/MedicationInventory/MedicationInventoryRepository.php
@@ -75,15 +75,17 @@ class MedicationInventoryRepository implements MedicationInventoryRepositoryInte
         $paginated = $query->latest()->paginate(3, ['*'], 'page', $page);
         
         // $paginated->getCollection()->transform($transformItem);
-        return MedicationInventoryResource::collection($paginated)->additional([
-        'meta' => [
-            'current_page' => $paginated->currentPage(),
-            'last_page' => $paginated->lastPage(),
-            'per_page' => $paginated->perPage(),
-            'total' => $paginated->total(),
-        ]
-    ]);
-         
+        $medical = MedicationInventoryResource::collection($paginated);
+
+     $data =   [
+    'data' => $medical,
+    'meta' => [
+        'current_page' => $paginated->currentPage(),
+        'last_page' => $paginated->lastPage(),
+        'per_page' => $paginated->perPage(),
+        'total' => $paginated->total(),
+    ]];
+      return $data;   
     }
 
 
