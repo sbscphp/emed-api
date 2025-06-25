e4d1faec9817c55821544eade293bc9993d7c8c5
StephenOkpeku <Stephen@appentus.in>
2025-06-25 18:09:20 +0100

    update

--- app/Http/Controllers/v1/Admin/RecordManagementController.php
+++ app/Http/Controllers/v1/Admin/RecordManagementController.php
@@ -565,7 +565,7 @@ class RecordManagementController extends Controller
             ]);
 
 
-            $serviceDate = $records->data->records ?? null;
+            $serviceDate = $records->data ?? null;
 
                
 
