d99b0a82acca00c2137b4d854fee33d7c67b55ff
StephenOkpeku <Stephen@appentus.in>
2025-06-27 11:14:42 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'https://emed-application.vercel.app',
+        'http://127.0.0.1:5500',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
