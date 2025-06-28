297acf592d292059978d2e4f4956fd1eafe0721f
StephenOkpeku <Stephen@appentus.in>
2025-06-28 10:11:07 +0100

    update

--- app/Http/Middleware/RecordsAccessMiddleware.php
+++ app/Http/Middleware/RecordsAccessMiddleware.php
@@ -25,17 +25,33 @@ class RecordsAccessMiddleware
             return JsonResponser::send(true, 'Authentication required. Please sign in.', [], 401);
         }
 
-        if (!$user->relationLoaded('roles')) {
+    
+
+        //   $role = Role::where('display_name', $user->role)->first();
+
+        // if(!$role){
+        //     return JsonResponser::send(true, 'Access Denied: Admin or Super Admin role required.', [], 403);   
+        // }
+
+        // if (!$role?->name ==  'admin' || !$role?->name ==  'super_admin' || !$role?->name ==  'record') {
+        //     ErrorLog::create([
+        //         'causer'        => $user->id,
+        //         'model'         => 'Permission',
+        //         'error_message' => "Unauthorized access attempt by {$user->fullname}",
+        //         'request_url'   => $request->fullUrl(),
+        //         'request_method' => $request->method(),
+        //         'request_ip'    => $request->ip(),
+        //         'user_agent'    => $request->header('User-Agent'),
+        //     ]);
+
+        //     return JsonResponser::send(true, 'Access Denied: You do not have the permission.', [], 403);
+        // }
+
+     if (!$user->relationLoaded('roles')) {
             $user->load('roles');
         }
 
-          $role = Role::where('display_name', $user->role)->first();
-
-        if(!$role){
-            return JsonResponser::send(true, 'Access Denied: Admin or Super Admin role required.', [], 403);   
-        }
-
-        if (!$role?->name ==  'admin' || !$role?->name ==  'super_admin' || !$role?->name ==  'record') {
+        if (!$user->hasRole(['admin', 'super_admin', 'record'])) {
             ErrorLog::create([
                 'causer'        => $user->id,
                 'model'         => 'Permission',
