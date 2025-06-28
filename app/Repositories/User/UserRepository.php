1e39906dea2ffbfec8582c07de8aaa68d0231483
StephenOkpeku <Stephen@appentus.in>
2025-06-28 09:41:34 +0100

    update

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -117,7 +117,7 @@ class UserRepository implements UserRepositoryInterface
      */
     public function find(int $id, array $selectAttrs = [])
     {
-         dd(json_encode($id));
+         dd(json_encode(is_string($id)));
      $columns = !empty($selectAttrs) ? $selectAttrs : ['*'];
       return User::select($columns)->find($id);
        // return User::select($selectAttrs ? $selectAttrs : '*')->find($id);
