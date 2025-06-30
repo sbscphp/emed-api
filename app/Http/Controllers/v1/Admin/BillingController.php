20cbbb7fd97c316366f5e460ab6bd4e10fc6b34c
StephenOkpeku <Stephen@appentus.in>
2025-06-30 09:58:58 +0100

    update

--- app/Http/Controllers/v1/Admin/BillingController.php
+++ app/Http/Controllers/v1/Admin/BillingController.php
@@ -12,7 +12,7 @@ use App\Services\User\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\User;
 class BillingController extends Controller
 {
     protected $billingService;
@@ -54,8 +54,8 @@ class BillingController extends Controller
 
         try {
             $currentUser = Auth::user();
-            $user = $this->userService->find($currentUser->id);
-
+            //$user = $this->userService->find($currentUser->id);
+            $user = User::on('tenant')->where('email', $currentUser['email'])->first();
             $validated = array_merge($request->validated(), [
                 'created_by' => $currentUser->id,
             ]);
