72b93f0347134d4d343b9886a508d2eec4a25794
StephenOkpeku <Stephen@appentus.in>
2025-07-02 15:20:40 +0100

    update

--- database/seeders/MySqlDumpSeeder.php
+++ database/seeders/MySqlDumpSeeder.php
@@ -12,15 +12,63 @@ class MySqlDumpSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-     $sqlPath = database_path('sql/world.sql');
+//     public function run(): void
+//     {
+//  DB::statement('USE '.env('DB_DATABASE'));
+// DB::statement('SET SQL_SAFE_UPDATES = 0');
+// DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+// DB::statement("SET SESSION sql_mode = ''");
 
-        if (!File::exists($sqlPath)) {
-            throw new \Exception("SQL file not found at: $sqlPath");
-        }
+//     $sqlPath = database_path('sql/world.sql');
 
-        $sql = File::get($sqlPath);
-        DB::unprepared($sql);
+//     if (!File::exists($sqlPath)) {
+//         throw new \Exception("SQL file not found at: $sqlPath");
+//     }
+
+//     $sql = File::get($sqlPath);
+
+//     // Remove versioned MySQL comments like /*!40101 SET ...
+//     $sql = preg_replace('/\/\*![0-9]+ .*?\*\//s', '', $sql);
+
+//     // Split SQL into individual statements
+//     $statements = array_filter(array_map('trim', explode(';', $sql)));
+
+//     foreach ($statements as $statement) {
+//         if (!empty($statement)) {
+//             DB::unprepared($statement . ';');
+//         }
+//     }
+
+//         }
+
+
+
+public function run(): void
+{
+    DB::statement('USE ' . env('DB_DATABASE'));
+    DB::statement('SET SQL_SAFE_UPDATES = 0');
+    DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+    DB::statement("SET SESSION sql_mode = ''");
+
+    $remoteUrl = 'https://verdant-nasturtium-ccf81a.netlify.app/world.sql';
+
+    // Download the SQL file content
+    $sql = file_get_contents($remoteUrl);
+    if ($sql === false) {
+        throw new \Exception("Failed to download SQL from: $remoteUrl");
     }
+
+    // Remove versioned MySQL comments like /*!40101 SET ...
+    $sql = preg_replace('/\/\*![0-9]+ .*?\*\//s', '', $sql);
+
+    // Split SQL into individual statements
+    $statements = array_filter(array_map('trim', explode(';', $sql)));
+
+    foreach ($statements as $statement) {
+        if (!empty($statement)) {
+            DB::unprepared($statement . ';');
+        }
+    }
+}
+
 }
