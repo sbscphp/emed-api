d6d17afea842489533337a656c4b4a13147fe960
StephenOkpeku <Stephen@appentus.in>
2025-07-08 16:17:41 +0100

    update

--- app/Http/Controllers/v1/Admin/LabController.php
+++ app/Http/Controllers/v1/Admin/LabController.php
@@ -40,48 +40,41 @@ class LabController extends Controller
 
     public function allLabRecords(Request $request)
     {
-        //try {
-        $search = $request->search;
-        $status = $request->status;
-        $paymentStatus = $request->payment_status;
-        $paginate = $request->paginate ?? false;
-        $perPage = $request->perPage;
-        $from = $request->from;
-        $to  = $request->to;
-        $export = $request->export;
-        DB::connection('tenant')->beginTransaction();
-        $currentUser = Auth::user();
-        // $user = $this->userService->find($currentUser->id);
-        $user = User::on('tenant')->where('email', $currentUser['email'])->first();
+        try {
+            $search = $request->search;
+            $status = $request->status;
+            $paymentStatus = $request->payment_status;
+            $paginate = $request->paginate ?? false;
+            $perPage = $request->perPage;
+            $from = $request->from;
+            $to  = $request->to;
+            $export = $request->export;
+            DB::connection('tenant')->beginTransaction();
+            $currentUser = Auth::user();
+            // $user = $this->userService->find($currentUser->id);
+            $user = User::on('tenant')->where('email', $currentUser['email'])->first();
 
-        if (!$user) {
-            return JsonResponser::send(true, 'User not found.', null, 204);
+            if (!$user) {
+                return JsonResponser::send(true, 'User not found.', null, 204);
+            }
+
+
+            $labRecords = $this->laboratoryService->getAllLabRecords($search, $status, $paginate, $paymentStatus, $perPage, $export, $from, $to);
+
+            if ($labRecords->count() == 0 && !$paginate) {
+
+                return JsonResponser::send(false, "Record(s) not found.", $labRecords, 200);
+            }
+
+            $response = [
+                'records' => $labRecords,
+                'total' => collect($labRecords)->count()
+            ];
+
+            return JsonResponser::send(false, 'Record(s) found successfully.', $response, 200);
+        } catch (Throwable $th) {
+            return JsonResponser::send(true, 'Internal server error.', [], 500, $th);
         }
-
-
-        $labRecords = $this->laboratoryService->getAllLabRecords($search, $status, $paginate, $paymentStatus, $perPage, $export, $from, $to);
-
-        if ($labRecords->count() == 0 && !$paginate) {
-            // return response()->json([
-            //     "error" => false,
-            //     "message" => "Record(s) not found.",
-            //     "data" => $labRecords
-            // ]);
-            return JsonResponser::send(false, "Record(s) not found.", $labRecords, 200);
-        }
-
-
-        // return response()->json([$labRecords->count(), $paginate]);
-
-        // $response = [
-        //     'records' => $labRecords,
-        //     'total' => collect($labRecords)->count()
-        // ];
-
-        // return JsonResponser::send(false, 'Record(s) found successfully.', $response, 200);
-        // } catch (Throwable $th) {
-        //     return JsonResponser::send(true, 'Internal server error.', [], 500, $th);
-        // }
     }
 
     public function stats()
