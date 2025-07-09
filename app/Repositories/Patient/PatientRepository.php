b0c9fcfc92ebefc7ef04aca6fadbd81eb4aa011b
StephenOkpeku <Stephen@appentus.in>
2025-07-09 14:16:40 +0100

    update

--- app/Repositories/Patient/PatientRepository.php
+++ app/Repositories/Patient/PatientRepository.php
@@ -165,8 +165,8 @@ class PatientRepository implements PatientInterface
     public function getPatientReport(Request $request)
     {
         $request->validate([
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date',
+            'start_date' => 'nullable|date',
+            'end_date' => 'nullable|date|after_or_equal:start_date',
         ]);
 
         $start = Carbon::parse($request->start_date);
