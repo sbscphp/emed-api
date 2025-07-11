a31c8b3f12bccfdf2abdd67cb4d8e2bf020be389
StephenOkpeku <Stephen@appentus.in>
2025-07-12 00:50:50 +0100

    update

--- app/Exports/PatientExport.php
+++ app/Exports/PatientExport.php
@@ -3,33 +3,34 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Facades\Excel;
+use App\Models\Patient;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
-class PatientExport implements FromCollection, WithMapping
+class PatientExport implements FromCollection
 {
 
     public function collection()
     {
-        return \App\Models\Patient::all();
+        return Patient::all();
     }
 
-    public function map($row): array
-    {
-        return [
-            $this->clean($row->firstname),
-            $this->clean($row->lastname),
-            $this->clean($row->email),
-            $this->clean($row->gender),
-            $this->clean($row->status),
-            // add more fields as needed...
-        ];
-    }
+    // public function map($row): array
+    // {
+    //     return [
+    //         $this->clean($row->firstname),
+    //         $this->clean($row->lastname),
+    //         $this->clean($row->email),
+    //         $this->clean($row->gender),
+    //         $this->clean($row->status),
+    //         // add more fields as needed...
+    //     ];
+    // }
 
-    private function clean($value)
-    {
-        return is_string($value)
-            ? mb_convert_encoding($value, 'UTF-8', 'UTF-8')
-            : $value;
-    }
+    // private function clean($value)
+    // {
+    //     return is_string($value)
+    //         ? mb_convert_encoding($value, 'UTF-8', 'UTF-8')
+    //         : $value;
+    // }
 }
