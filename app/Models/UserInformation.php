324106af4bce7d9ef93d3aec7a0ff51ec02a2014
StephenOkpeku <Stephen@appentus.in>
2025-07-11 12:28:49 +0100

    update

--- app/Models/UserInformation.php
+++ app/Models/UserInformation.php
@@ -10,6 +10,6 @@ class UserInformation extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class);
     }
 }
