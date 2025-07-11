b4044ce0030a18c9ad6674fc0bd4d1c260343ca4
StephenOkpeku <Stephen@appentus.in>
2025-07-11 22:00:08 +0100

    update

--- app/Services/Patient/PatientService.php
+++ app/Services/Patient/PatientService.php
@@ -183,11 +183,11 @@ class PatientService
                 return ExportHelper::downloadPdf(Patient::get()->toArray(),  'patient.pdf');
             } else if ($export == 'csv') {
                 //  return ExportHelper::streamCsv(Patient::get()->toArray(), null, 'patient.csv');
-                $patient = Patient::all();
-                $data = PatientResourceExport::collection($patient)->resolve();
-                //  return ExportHelper::streamCsv($data, null, 'patient_' . now()->format('Ymd_His') . '.csv');
-                //return Excel::download(new PatientExport($patient), 'patients.xlsx');
-                return Excel::download(Patient::get()->toArray(), 'users.xlsx');
+                // $patient = Patient::all();
+                // $data = PatientResourceExport::collection($patient)->resolve();
+                // //  return ExportHelper::streamCsv($data, null, 'patient_' . now()->format('Ymd_His') . '.csv');
+                // //return Excel::download(new PatientExport($patient), 'patients.xlsx');
+                // return Excel::download(Patient::get()->toArray(), 'users.xlsx');
 
                 //return ExportHelper::streamCsv($data);
                 // $csv = new Csv($data);
