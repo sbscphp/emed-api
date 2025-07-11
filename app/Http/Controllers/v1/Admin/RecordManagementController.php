61d7d54a293a2a15ea5483385848155cac837dff
StephenOkpeku <Stephen@appentus.in>
2025-07-11 16:08:30 +0100

    update

--- app/Http/Controllers/v1/Admin/RecordManagementController.php
+++ app/Http/Controllers/v1/Admin/RecordManagementController.php
@@ -569,37 +569,38 @@ class RecordManagementController extends Controller
             return JsonResponser::send(false, 'Record(s) not found.', null, 200);
         }
 
-        $records->load([
-            'service',
-            'visits_recent',
-            // 'visits' => function ($query) {
-            //     $query->select(
-            //         'id',
-            //         'patient_id',
-            //         'visitno',
-            //         'stage',
-            //         'status',
-            //         'arrival_date',
-            //         'departure_date',
-            //         'visit_date',
-            //         'created_at'
-            //     );
-            // }
-        ]);
+
+
+        if (empty($export)) {
+            $records->load([
+                'service',
+                'visits_recent',
+                // 'visits' => function ($query) {
+                //     $query->select(
+                //         'id',
+                //         'patient_id',
+                //         'visitno',
+                //         'stage',
+                //         'status',
+                //         'arrival_date',
+                //         'departure_date',
+                //         'visit_date',
+                //         'created_at'
+                //     );
+                // }
+            ]);
 
 
 
+            $summary = $this->patientService->getRecordStats();
+            return JsonResponser::send(false, 'Record(s) found successfully.', [
+                // 'records' => $records,
+                'records' => collect($records),
+                'summary' => $summary,
+            ], 200);
+        }
 
 
-
-
-
-        $summary = $this->patientService->getRecordStats();
-        return JsonResponser::send(false, 'Record(s) found successfully.', [
-            // 'records' => $records,
-            'records' => collect($records),
-            'summary' => $summary,
-        ], 200);
         // } catch (\Throwable $th) {
         //     return JsonResponser::send(true, 'Internal server error.', [], 500, $th);
         // }
