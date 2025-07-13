6e275c14b7be70a7027c6ba66548b49de6a77813
StephenOkpeku <Stephen@appentus.in>
2025-07-13 13:40:27 +0100

    update

--- app/Http/Controllers/v1/Admin/RecordManagementController.php
+++ app/Http/Controllers/v1/Admin/RecordManagementController.php
@@ -562,6 +562,91 @@ class RecordManagementController extends Controller
 
 
 
+
+        if ($export == 'csv') {
+            $fileName = 'patients.csv';
+
+            $headers = [
+                'Content-Type' => 'text/csv',
+                'Content-Disposition' => 'attachment; filename="patients.csv"',
+            ];
+
+            $columns = [
+                'firstname',
+                'lastname',
+                'dob',
+                'age',
+                'gender',
+                'bloodgroup',
+                'genotype',
+                'email',
+                'patient_type',
+                'marital_status',
+                'phoneno',
+                'visitno',
+                'occupation',
+                'homeaddress',
+                'companyaddress',
+                'religion',
+                'stateoforigin',
+                'lga',
+                'tribe',
+                'cardno',
+                'receiptno',
+                'status',
+                'service_id',
+                'arrival_time',
+                'depature_time',
+                'patientno'
+
+            ];
+
+            $callback = function () use ($columns) {
+                $file = fopen('php://output', 'w');
+                fputcsv($file, $columns);
+
+                // $patients = Patient::all(); // Adjust to your fields
+
+                $patients  = Patient::get()->toArray();
+                foreach ($patients as $patient) {
+                    fputcsv($file, [
+                        $patient['firstname'],
+                        $patient['lastname'],
+                        $patient['dob'],
+                        $patient['age'],
+                        $patient['gender'],
+                        $patient['bloodgroup'],
+                        $patient['genotype'],
+                        $patient['email'],
+                        $patient['patient_type'],
+                        $patient['marital_status'],
+                        $patient['phoneno'],
+                        $patient['visitno'],
+                        $patient['occupation'],
+                        $patient['homeaddress'],
+                        $patient['companyaddress'],
+                        $patient['religion'],
+                        $patient['stateoforigin'],
+                        $patient['lga'],
+                        $patient['tribe'],
+                        $patient['cardno'],
+                        $patient['receiptno'],
+                        $patient['status'],
+                        $patient['service_id'],
+                        $patient['arrival_time'],
+                        $patient['depature_time'],
+                        $patient['patientno']
+                    ]);
+                }
+
+                fclose($file);
+            };
+
+            return response()->stream($callback, 200, $headers);
+        }
+
+
+
         if (is_null($user)) {
             return JsonResponser::send(false, 'User not found.', null, 200);
         }
--- app/Services/Patient/PatientService.php
+++ app/Services/Patient/PatientService.php
@@ -362,101 +362,24 @@ class PatientService
                 return $dompdf->stream('patient_report_log.pdf', [
                     'Attachment' => 1
                 ]);
-            } else if ($export == 'csv') {
-                //  return ExportHelper::streamCsv($exportData, null, 'patients_' . now()->format('Ymd_His') . '.csv');
-                // return ExportHelper::streamCsv($data);
-                // $csv = new Csv($data);
-                //   PatientExport
-                // $data = Patient::get()->toArray();
-                //dd(json_encode([$export, $data]));
-                // return Excel::download(new PatientExport, 'patients.csv');
-
-                //return Excel::download(new PatientExport, 'patients.csv', ExcelFormat::CSV);
-
-
-                // return ExportHelper::streamCsv($data, null, 'patient_' . now()->format('Ymd_His') . '.csv');
-                // return Excel::download(new PatientExport, 'patients.csv', ExcelFormat::CSV);
-
-                $fileName = 'patients.csv';
-
-                $headers = [
-                    'Content-Type' => 'text/csv',
-                    'Content-Disposition' => 'attachment; filename="patients.csv"',
-                ];
-
-                $columns = [
-                    'firstname',
-                    'lastname',
-                    'dob',
-                    'age',
-                    'gender',
-                    'bloodgroup',
-                    'genotype',
-                    'email',
-                    'patient_type',
-                    'marital_status',
-                    'phoneno',
-                    'visitno',
-                    'occupation',
-                    'homeaddress',
-                    'companyaddress',
-                    'religion',
-                    'stateoforigin',
-                    'lga',
-                    'tribe',
-                    'cardno',
-                    'receiptno',
-                    'status',
-                    'service_id',
-                    'arrival_time',
-                    'depature_time',
-                    'patientno'
-
-                ];
-
-                $callback = function () use ($columns) {
-                    $file = fopen('php://output', 'w');
-                    fputcsv($file, $columns);
-
-                    // $patients = Patient::all(); // Adjust to your fields
-
-                    $patients  = Patient::get()->toArray();
-                    foreach ($patients as $patient) {
-                        fputcsv($file, [
-                            $patient['firstname'],
-                            $patient['lastname'],
-                            $patient['dob'],
-                            $patient['age'],
-                            $patient['gender'],
-                            $patient['bloodgroup'],
-                            $patient['genotype'],
-                            $patient['email'],
-                            $patient['patient_type'],
-                            $patient['marital_status'],
-                            $patient['phoneno'],
-                            $patient['visitno'],
-                            $patient['occupation'],
-                            $patient['homeaddress'],
-                            $patient['companyaddress'],
-                            $patient['religion'],
-                            $patient['stateoforigin'],
-                            $patient['lga'],
-                            $patient['tribe'],
-                            $patient['cardno'],
-                            $patient['receiptno'],
-                            $patient['status'],
-                            $patient['service_id'],
-                            $patient['arrival_time'],
-                            $patient['depature_time'],
-                            $patient['patientno']
-                        ]);
-                    }
-
-                    fclose($file);
-                };
-
-                return response()->stream($callback, 200, $headers);
             }
+
+            // else if ($export == 'csv') {
+            //     //  return ExportHelper::streamCsv($exportData, null, 'patients_' . now()->format('Ymd_His') . '.csv');
+            //     // return ExportHelper::streamCsv($data);
+            //     // $csv = new Csv($data);
+            //     //   PatientExport
+            //     // $data = Patient::get()->toArray();
+            //     //dd(json_encode([$export, $data]));
+            //     // return Excel::download(new PatientExport, 'patients.csv');
+
+            //     //return Excel::download(new PatientExport, 'patients.csv', ExcelFormat::CSV);
+
+
+            //     // return ExportHelper::streamCsv($data, null, 'patient_' . now()->format('Ymd_His') . '.csv');
+            //     // return Excel::download(new PatientExport, 'patients.csv', ExcelFormat::CSV);
+
+            // }
         }
 
         $query->when($from && $to, function ($q) use ($from, $to) {
