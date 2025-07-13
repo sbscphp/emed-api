c5325f61f324214187abd0af0b550dbb291339cf
StephenOkpeku <Stephen@appentus.in>
2025-07-13 20:12:57 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -10,8 +10,8 @@ return [
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+    // 'exposed_headers' => [],
+    'exposed_headers' => ['Content-Disposition'],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
-
