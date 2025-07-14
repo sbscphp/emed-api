bb296fb66f34bd6d9846cfebc2da4914551985f9
StephenOkpeku <Stephen@appentus.in>
2025-07-14 16:34:00 +0100

    update

--- app/Repositories/MedicationInventory/MedicationInventoryRepository.php
+++ app/Repositories/MedicationInventory/MedicationInventoryRepository.php
@@ -26,15 +26,15 @@ class MedicationInventoryRepository implements MedicationInventoryRepositoryInte
         if (!empty($filters['search'])) {
             $search = $filters['search'];
             $query->where(function ($q) use ($search) {
-                $q->where('batch_no', 'like', "%$search%")
-                    ->orWhere('shipment_status', 'like', "%$search%")
+                $q->where('batch_no', 'like', "%{$search}%")
+                    ->orWhere('shipment_status', 'like', "%{$search}%")
                     ->orWhereHas('medication', function ($mq) use ($search) {
-                        $mq->where('medicine_name', 'like', "%$search%")
-                            ->orWhere('brand_name', 'like', "%$search%")
-                            ->orWhere('generic_name', 'like', "%$search%");
+                        $mq->where('medicine_name', 'like', "%{$search}%")
+                            ->orWhere('brand_name', 'like', "%{$search}%")
+                            ->orWhere('generic_name', 'like', "%{$search}%");
                     })
                     ->orWhereHas('pharmacy', function ($pq) use ($search) {
-                        $pq->where('name', 'like', "%$search%");
+                        $pq->where('name', 'like', "%{$search}%");
                     });
             });
         }
