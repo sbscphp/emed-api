49843e3f2d10bb5bdb408665156e4669fa1ca928
StephenOkpeku <Stephen@appentus.in>
2025-07-16 15:09:07 +0100

    update

--- app/Http/Controllers/v1/Admin/AuditLogController.php
+++ app/Http/Controllers/v1/Admin/AuditLogController.php
@@ -150,13 +150,15 @@ class AuditLogController extends Controller
             // $search = $validate['search'];
             config(['database.default' => 'tenant']);
             DB::connection('tenant');
-            $medical_log = AuditLog::
-                // whereIn('action_type', [
-                //     'App\Models\MedicalHistory',
-                //     'App\Models\Medication',
-                //     'App\Models\MedicineType'
-                // ])
-                when(!empty($validate['search']), function ($query, $validate) {
+            $medical_log = AuditLog::whereIn('action_type', [
+                'App\Models\MedicalHistory',
+                'App\Models\Medication',
+                'App\Models\MedicineType',
+                'Models\MedicineType',
+                'Models\Pharmacy',
+                'Models\MedicineInventory',
+            ])
+                ->when(!empty($validate['search']), function ($query, $validate) {
                     $query->where('action_type', 'LIKE', "%{$validate['search']}%");
                 })->get();
 
