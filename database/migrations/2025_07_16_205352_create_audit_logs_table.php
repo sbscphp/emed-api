ed652df2f6474f4024d7d870b7050826cb61fb9d
StephenOkpeku <Stephen@appentus.in>
2025-07-17 22:03:21 +0100

    update

--- database/migrations/2025_07_16_205352_create_audit_logs_table.php
+++ database/migrations/2025_07_16_205352_create_audit_logs_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('medicine__logs');
+        Schema::dropIfExists('audit_logs');
     }
 };
