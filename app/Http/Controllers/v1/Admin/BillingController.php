f154dfaca38b0a2ac7868645dee4b9ac4190a23f
StephenOkpeku <Stephen@appentus.in>
2025-07-18 14:43:39 +0100

    update

--- app/Http/Controllers/v1/Admin/BillingController.php
+++ app/Http/Controllers/v1/Admin/BillingController.php
@@ -348,15 +348,15 @@ class BillingController extends Controller
 
     public function billingsummary()
     {
-        try {
-            DB::connection('tenant')->beginTransaction();
-            $billingSummaries = BillingLog::select('service_unit_id', DB::raw('SUM(grand_total) as total_billing'))
-                ->groupBy('service_unit_id')
-                ->with('serviceUnit')
-                ->get();
-            return JsonResponser::send(false, 'Billing summary fetched successfully.', $billingSummaries);
-        } catch (\Throwable $th) {
-            return JsonResponser::send(true, 'Error fetching billing summary stats.', [], 500, $th);
-        }
+        // try {
+        DB::connection('tenant')->beginTransaction();
+        $billingSummaries = BillingLog::select('service_unit_id', DB::raw('SUM(grand_total) as total_billing'))
+            ->groupBy('service_unit_id')
+            ->with('serviceUnit')
+            ->get();
+        return JsonResponser::send(false, 'Billing summary fetched successfully.', $billingSummaries);
+        // } catch (\Throwable $th) {
+        //     return JsonResponser::send(true, 'Error fetching billing summary stats.', [], 500, $th);
+        // }
     }
 }
