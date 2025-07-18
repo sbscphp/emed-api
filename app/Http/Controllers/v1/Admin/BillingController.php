f19655ef1a7e2ba0c1f78e3eb4ffc7de8f8b35d7
StephenOkpeku <Stephen@appentus.in>
2025-07-18 15:05:56 +0100

    update

--- app/Http/Controllers/v1/Admin/BillingController.php
+++ app/Http/Controllers/v1/Admin/BillingController.php
@@ -340,7 +340,25 @@ class BillingController extends Controller
             DB::connection('tenant')->beginTransaction();
             // $billingSummaries = BillingLog::with('serviceUnit')
             //     ->get();
-            $billingSummaries = BillingLog::select('service_unit_id', DB::raw('SUM(grand_total) as total_billing'))
+            $billingSummaries = BillingLog::select(
+                'service_unit_id',
+                DB::raw('SUM(grand_total) as total_billing'),
+                'invoice_number',
+                'visit_id',
+                'patient_id',
+                'patient_name',
+                'billing_date',
+                'service_type_id',
+                'service_unit_id',
+                'item_name',
+                'unit_price',
+                'quantity',
+                'payment_status',
+                'deposit_amount',
+                'payment_method',
+                'sub_total',
+                'tax_amount',
+            )
                 ->groupBy('service_unit_id')
                 ->with('serviceUnit')
                 ->get();
