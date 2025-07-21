4940b1d01b72f713692bd0f43ffffdbfff9556e7
StephenOkpeku <Stephen@appentus.in>
2025-07-21 20:11:11 +0100

    update

--- app/Http/Controllers/v1/Admin/BillingController.php
+++ app/Http/Controllers/v1/Admin/BillingController.php
@@ -447,22 +447,22 @@ class BillingController extends Controller
         ]);
 
         $data = $this->billingService->pharmacy_list($validated);
-        $pharm =  Pharmacy::with(["pharmacist", 'treatments_one.patient.visits_recent.billingLogsForPatient'])->get();
-        if (count($pharm) == 0) {
-            return JsonResponser::send(true, 'No Data.', [], 500);
-        }
-        if (!empty($validated['export'])) {
-            $export =  $validated['export'];
-            $exportData = PharmacyResourceList::collection($pharm)->resolve();
+        // $pharm =  Pharmacy::with(["pharmacist", 'treatments_one.patient.visits_recent.billingLogsForPatient'])->get();
+        // if (count($pharm) == 0) {
+        //     return JsonResponser::send(true, 'No Data.', [], 500);
+        // }
+        // if (!empty($validated['export'])) {
+        //     $export =  $validated['export'];
+        //     $exportData = PharmacyResourceList::collection($pharm)->resolve();
 
-            if ($export === 'csv') {
-                return ExportHelper::streamCsv($exportData, null, 'pharmacy.csv');
-            }
+        //     if ($export === 'csv') {
+        //         return ExportHelper::streamCsv($exportData, null, 'pharmacy.csv');
+        //     }
 
-            if ($export === 'pdf') {
-                return ExportHelper::downloadPdf($exportData, 'pharmacy.pdf');
-            }
-        }
+        //     if ($export === 'pdf') {
+        //         return ExportHelper::downloadPdf($exportData, 'pharmacy.pdf');
+        //     }
+        // }
         return JsonResponser::send(false, 'Billing stats fetched successfully.', $data);
     }
 
