ee3ff72a156c8bc86a067f57e26b58d9cc4f5795
StephenOkpeku <Stephen@appentus.in>
2025-07-22 20:01:28 +0100

    update

--- app/Http/Resources/MedicationInventoryResource.php
+++ app/Http/Resources/MedicationInventoryResource.php
@@ -12,33 +12,33 @@ class MedicationInventoryResource extends JsonResource
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array 
+    public function toArray(Request $request): array
     {
 
 
 
-    $medication = optional($this->medication);
-    $pharmacy = optional($this->pharmacy);
-    $sellingPrice = $medication->selling_price ?? 0;
-    $totalPrice = $this->received_qty * $sellingPrice; 
+        $medication = optional($this->medication);
+        $pharmacy = optional($this->pharmacy);
+        $sellingPrice = $medication->selling_price ?? 0;
+        $totalPrice = $this->received_qty * $sellingPrice;
 
-    return [
-    'id' => $this->id, 
-    'batch_no' => $this->batch_no, 
-    'shipment_no'=>$this->shipment_no,
-    'shipment_status' => $this->shipment_status,
-    'medicine_name' => $medication->medicine_name,
-    'vendor' => $this->vendor,
-    'manufacturer'=>$medication->manufacturer,
-    'brand_name' => $this->brand_name,
-    'generic_name' => $medication->generic_name,
-    'pharmacy' => $pharmacy->name,
-    'medicine_type'=> $medication->medicine_type,
-    'received_qty' => $this->received_qty,
-    'price' => (float)$this->price,
-    'selling_price' => (float) $sellingPrice,
-    'total_price' => (float) $totalPrice,
-    'createdAt' => optional($this->created_at)->toDateTimeString(),
-    ];
+        return [
+            'id' => $this->id,
+            'batch_no' => $this->batch_no,
+            'shipment_no' => $this->shipment_no,
+            'shipment_status' => $this->shipment_status,
+            'medicine_name' => $medication->medicine_name,
+            'vendor' => $this->vendor?->vendor_name,
+            'manufacturer' => $medication->manufacturer,
+            'brand_name' => $this->brand_name,
+            'generic_name' => $medication->generic_name,
+            'pharmacy' => $pharmacy->name,
+            'medicine_type' => $medication->medicine_type,
+            'received_qty' => $this->received_qty,
+            'price' => (float)$this->price,
+            'selling_price' => (float) $sellingPrice,
+            'total_price' => (float) $totalPrice,
+            'createdAt' => optional($this->created_at)->toDateTimeString(),
+        ];
     }
 }
