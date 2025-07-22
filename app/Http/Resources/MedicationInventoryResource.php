7e873096452e2fd3d17740dfc92ca434e05053e3
StephenOkpeku <Stephen@appentus.in>
2025-07-22 21:06:53 +0100

    update

--- app/Http/Resources/MedicationInventoryResource.php
+++ app/Http/Resources/MedicationInventoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Vendor;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -21,14 +22,14 @@ class MedicationInventoryResource extends JsonResource
         $pharmacy = optional($this->pharmacy);
         $sellingPrice = $medication->selling_price ?? 0;
         $totalPrice = $this->received_qty * $sellingPrice;
-
+        $vendor  = $this->vendor_id ? Vendor::find(intval($this->vendor_id)) : null;
         return [
             'id' => $this->id,
             'batch_no' => $this->batch_no,
             'shipment_no' => $this->shipment_no,
             'shipment_status' => $this->shipment_status,
             'medicine_name' => $medication->medicine_name,
-            'vendor' => $this->vendor->vendor_name ?? "",
+            'vendor' => $vendor->vendor_name ?? "",
             'manufacturer' => $medication->manufacturer,
             'brand_name' => $this->brand_name,
             'generic_name' => $medication->generic_name,
