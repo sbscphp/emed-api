47acb9c04a9e8352e825f9ba5aee6af9d8d9bb63
StephenOkpeku <Stephen@appentus.in>
2025-07-23 14:54:51 +0100

    update

--- app/Models/Radiology_lab_patient_examination.php
+++ app/Models/Radiology_lab_patient_examination.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Radiology_lab_patient_examination extends Model
 {
-
+    protected $guarded = ['id'];
+    protected $connection = 'tenant';
     protected $fillable = [
         'radiology_lab_patients_id',
         'examination',
