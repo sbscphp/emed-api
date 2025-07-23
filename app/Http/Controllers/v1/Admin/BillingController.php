cd19a13f250ff15dc73caf59baa89dae03701bfd
StephenOkpeku <Stephen@appentus.in>
2025-07-23 16:37:15 +0100

    update

--- app/Http/Controllers/v1/Admin/BillingController.php
+++ app/Http/Controllers/v1/Admin/BillingController.php
@@ -851,14 +851,14 @@ class BillingController extends Controller
 
     public function payment_billing_daft(Request $request)
     {
-        try {
-            $validated =  $request->validate([
-                'patient_visits_id' => "nullable|numeric"
-            ]);
-            $data = PatientVisit::with(['patient.laboratory', 'patient.pharmacy', 'patient.radiology', 'patient.consultations', 'patient.billingLogsForPatient'])->where('patient_visits_id', intval($validated['patient_visits_id']))->first();
-            return JsonResponser::send(false, 'Billing stats fetched successfully.', $data);
-        } catch (\Exception $e) {
-            return JsonResponser::send(true, 'Error fetching billing stats.', [], 500, $e);
-        }
+        // try {
+        $validated =  $request->validate([
+            'patient_visits_id' => "nullable|numeric"
+        ]);
+        $data = PatientVisit::with(['patient.laboratory', 'patient.pharmacy', 'patient.radiology', 'patient.consultations', 'patient.billingLogsForPatient'])->where('patient_visits_id', intval($validated['patient_visits_id']))->first();
+        return JsonResponser::send(false, 'Billing stats fetched successfully.', $data);
+        // } catch (\Exception $e) {
+        //     return JsonResponser::send(true, 'Error fetching billing stats.', [], 500, $e);
+        // }
     }
 }
