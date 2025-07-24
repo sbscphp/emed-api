7d53cbecf988d87c4181415f65f01ef2381140e5
StephenOkpeku <Stephen@appentus.in>
2025-07-24 12:56:14 +0100

    update

--- app/Http/Controllers/v1/Admin/MainDashBoardStatsController.php
+++ app/Http/Controllers/v1/Admin/MainDashBoardStatsController.php
@@ -34,7 +34,7 @@ class MainDashBoardStatsController extends Controller
 
         try {
             $validated = $request->validate([
-                "filter_calender" => 'nullable|string|in:daily,monthly, yearly'
+                "filter_calender" => 'nullable|string|in:daily,monthly,yearly'
             ]);
 
             $validated['filter_calender'] ?? "daily";
