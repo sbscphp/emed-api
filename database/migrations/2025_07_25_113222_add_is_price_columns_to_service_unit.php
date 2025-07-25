6bffdbdd102e769e27935f8bc10bd4187f9773b9
StephenOkpeku <Stephen@appentus.in>
2025-07-25 13:01:51 +0100

    update

--- database/migrations/2025_07_25_113222_add_is_price_columns_to_service_unit.php
+++ database/migrations/2025_07_25_113222_add_is_price_columns_to_service_unit.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('service_unit', function (Blueprint $table) {
-            //
+            $table->decimal('price')->nullable();
         });
     }
 };
