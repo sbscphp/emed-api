4b25a6191d464c6fa342dcedf69ca892937ec6fe
StephenOkpeku <Stephen@appentus.in>
2025-07-30 15:40:42 +0100

    update

--- app/Http/Controllers/v1/Admin/RegistrationController.php
+++ app/Http/Controllers/v1/Admin/RegistrationController.php
@@ -303,8 +303,7 @@ class RegistrationController extends Controller
                 return JsonResponser::send(false, "Tenant {$data['name']} already exists.", [], 500);
             }
 
-            $isProduction = env('IS_PRODUCTION');
-            //app()->environment(['production', 'staging', 'qa']);
+            $isProduction = app()->environment(['production', 'staging', 'qa']);
             $tenantDatabase = $isProduction
                 ? 'jkpmjemy_tenant_john_hospital'
                 : 'tenant_' . Str::slug($data['name'], '_');
