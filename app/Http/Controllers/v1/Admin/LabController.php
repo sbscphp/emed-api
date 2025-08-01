57fb3eb91050d8a4e8af03bbb2731eed03882309
StephenOkpeku <Stephen@appentus.in>
2025-08-01 17:35:59 +0100

    update

--- app/Http/Controllers/v1/Admin/LabController.php
+++ app/Http/Controllers/v1/Admin/LabController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\v1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Laboratory;
 use App\Responser\JsonResponser;
 use App\Services\Consultation\ConsultationService;
 use App\Services\Laboratory\LaboratoryService;
@@ -65,9 +66,23 @@ class LabController extends Controller
 
                 return JsonResponser::send(false, "Record(s) not found.", $labRecords, 200);
             }
+            $query = Laboratory::query()
+                ->join('patients', 'patient_visit_lab.patient_id', '=', 'patients.id')
+                ->leftJoin('billing_logs', 'patient_visit_lab.patient_id', '=', 'billing_logs.patient_id')
+                ->select(
+                    'patient_visit_lab.*',
+                    'patients.firstname',
+                    'patients.lastname',
+                    'patients.patientno',
+                    'patients.cardno',
+                    'billing_logs.id as billing_id',
+                    'billing_logs.sub_total as billing_amount',
+                    'billing_logs.payment_status as billing_status'
+                )->get();
 
             $response = [
-                'records' => $labRecords,
+                'records' => $query,
+                // 'records' => $labRecords,
                 'total' => collect($labRecords)->count()
             ];
             DB::connection('tenant')->commit();
--- app/Repositories/Laboratory/LaboratoryRepository.php
+++ app/Repositories/Laboratory/LaboratoryRepository.php
@@ -238,8 +238,6 @@ class LaboratoryRepository implements LaboratoryInterface
             if ($export === 'pdf') {
                 return ExportHelper::downloadPdf($exportData->toArray(), 'lab-records.pdf');
             }
-
-            return $records;
         }
 
         // Optional pagination (uncomment if needed)
