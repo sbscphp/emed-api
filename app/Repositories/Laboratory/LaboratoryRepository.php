9a332e5908ccb2604d35835dc29704e863eb00bb
StephenOkpeku <Stephen@appentus.in>
2025-08-01 16:25:28 +0100

    update

--- app/Repositories/Laboratory/LaboratoryRepository.php
+++ app/Repositories/Laboratory/LaboratoryRepository.php
@@ -166,7 +166,6 @@ class LaboratoryRepository implements LaboratoryInterface
         // return $query->get();
         // //return $paginate ? $query->paginate($perPage ?? 10) : $query->get();
 
-        $paymentStatus = $paymentStatus ?? 'pending';
 
         // Start query
         $query = Laboratory::query()
