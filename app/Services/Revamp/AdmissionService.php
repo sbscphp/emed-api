5e597f44abcc5d504b1fcae660166565e84f60db
Aisimiyu Oluwadarasimi <aisimiyuoluwadara@gmail.com>
2026-06-19 14:56:44 +0100

    bug fixes

--- app/Services/Revamp/AdmissionService.php
+++ app/Services/Revamp/AdmissionService.php
@@ -463,7 +463,7 @@ class AdmissionService
                 $query->orderBy('created_at', 'ASC');
             })->when(($request['sort_by'] ?? null) === 'date_descending', function ($query) {
                 $query->orderBy('created_at', 'DESC');
-            })->with('patient', 'visit', 'drug', 'administeredBy');
+            })->with('patient', 'visit', 'drug', 'administeredBy:id,first_name,last_name,email');
 
         if (!empty($request['paginate'])) {
             return $query->orderBy('id', 'DESC')->paginate($request['limit'] ?? 15);
