8820c255c4688405db9b07479134e07fa1a2c7f4
Aisimiyu Oluwadarasimi <aisimiyuoluwadara@gmail.com>
2026-09-15 11:21:25 +0100

    added themes to mobile app

--- database/migrations/landlord/2026_09_15_100859_create_themes_table.php
+++ database/migrations/landlord/2026_09_15_100859_create_themes_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('themes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('tenant_id')->nullable();
             $table->string('name');
             $table->string('primary_color');
             $table->string('secondary_color');
